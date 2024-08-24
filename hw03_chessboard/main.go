3c707f4733e72a137caeddc9e1e6144bdf51fb41
freeloginname <Pechkin1@mail.ru>
2024-08-24 21:32:08 +0300

    linter fix

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -7,30 +7,30 @@ import (
 func main() {
 	size := 8
 	var board string
-	var cell_character string
+	var cellCharacter string
 	fmt.Println("Enter the board size... (8 is the default one)")
 	fmt.Scanf("%d \n", &size)
 	if size < 0 {
 		fmt.Println("Only natural numbers supported.")
 		return
 	}
-	for line := 0; line < size; line = line + 1 {
-		var odd_condition int
+	for line := 0; line < size; line++ {
+		var oddCondition int
 		if line%2 == 1 {
-			odd_condition = 1
+			oddCondition = 1
 		} else {
-			odd_condition = 0
+			oddCondition = 0
 		}
-		for cell := 0; cell < size; cell = cell + 1 {
-			if cell%2 == odd_condition {
-				cell_character = "#"
+		for cell := 0; cell < size; cell++ {
+			if cell%2 == oddCondition {
+				cellCharacter = "#"
 			} else {
-				cell_character = " "
+				cellCharacter = " "
 			}
 
-			board = board + cell_character
+			board += cellCharacter
 		}
-		board = board + "\n"
+		board += "\n"
 	}
 	fmt.Printf("Printing %d x %d chess board...\n", size, size)
 	fmt.Println(board)
