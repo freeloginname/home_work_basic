709360a4875938b29173e283df6a117d994982e4
freeloginname <Pechkin1@mail.ru>
2024-09-25 21:20:17 +0300

    linter fix

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -5,28 +5,19 @@ import (
 )
 
 func BinarySearch(array []int, target int) int {
-
 	low := 0
 	high := len(array) - 1
 	for low <= high {
 		mid := (low + high) / 2
 		fmt.Printf("low = %d, high = %d, mid = %d\n", low, high, mid)
-		if array[mid] == target {
+		switch {
+		case array[mid] == target:
 			return mid
-		} else if array[mid] < target {
-			low = mid + 1
-		} else {
+		case array[mid] > target:
 			high = mid - 1
+		case array[mid] < target:
+			low = mid + 1
 		}
 	}
 	return -1
-}
-
-func main() {
-
-	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	target := 5
-
-	index := BinarySearch(items, target)
-	fmt.Printf("Found %d at index %d\n", target, index)
 }
