9350f9c600afd58147933b6f2e243fdd1f0748f6
freeloginname <Pechkin1@mail.ru>
2024-10-26 15:11:06 +0300

    доработки

--- hw09_serialize/go.mod
+++ hw09_serialize/go.mod
@@ -1,3 +1,7 @@
-module github.com/fixme_my_friend/hw09_serialize
+module freeloginname/home_work_basic/hw09_serialize
 
-go 1.20
+go 1.21
+
+toolchain go1.22.5
+
+require google.golang.org/protobuf v1.35.1 // indirect
--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -1,5 +1,67 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Book struct {
+	Id     int
+	Title  string
+	Author string
+	Year   int
+	Size   int
+	Rate   float32
+}
+
+func (book *Book) Marshaller() ([]byte, error) {
+	return json.Marshal(book)
+}
+
+func (book *Book) Unmarshaller(data []byte) error {
+	err := json.Unmarshal(data, book)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func SliceMarshaller(books []Book) ([]byte, error) {
+	var result []byte
+	for book := range books {
+		currentBook, err := books[book].Marshaller()
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, currentBook...)
+	}
+	return result, nil
+}
+
+func SliceUnmarshaller(data []byte) ([]Book, error) {
+	var result []Book
+	err := json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
 func main() {
 	// Place your code here.
+	var book Book = Book{1, "title", "author", 1, 1, 1.0}
+
+	bytes, _ := book.Marshaller()
+	fmt.Println(bytes)
+	j := []byte(`{"Id":2,"Title":"title2","Author":"author2","Year":2,"Size":2,"Rate":2.0}`)
+	var book2 Book
+	_ = book2.Unmarshaller(j)
+	fmt.Println(book2)
+
+	result, _ := SliceMarshaller([]Book{{1, "title", "author", 1, 1, 1.0}, {2, "title2", "author2", 2, 2, 2.0}})
+	fmt.Println(result)
+
+	result2, _ := SliceUnmarshaller([]byte(`[{"Id":1,"Title":"title","Author":"author","Year":1,"Size":1,"Rate":1.0},{"Id":2,"Title":"title2","Author":"author2","Year":2,"Size":2,"Rate":2.0}]`))
+	fmt.Println(result2)
+
 }
